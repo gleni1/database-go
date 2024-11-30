3cccda505e10e1d7adc84d42fb786fa2957dc1e7
gleni1 <glenipoleshi@gmail.com>
2024-11-30 18:16:05 -0500

    Working on node structure initialization

--- main.go
+++ main.go
@@ -2,7 +2,78 @@ package main
 
 import (
   "fmt"
+  "encoding/binary"
 )
+
+const HEADER = 4
+
+const BTREE_PAGE_SIZE = 4096
+const BTREE_MAX_KEY_SIZE = 1000
+const BTREE_MAX_VAL_SIZE = 3000
+
+func init(){
+  node1max := HEADER + 8 + 2 + 4 + BTREE_MAX_KEY_SIZE + BTREE_MAX_VAL_SIZE
+  assert(node1max <= BTREE_PAGE_SIZE) // maximum KV
+}
+
+type BNode []byte 
+
+type BTree struct {
+  root uint64
+  get func(uint64) []byte 
+  new func([]byte) uint64
+  del func(uint64)
+}
+
+const (
+  BNODE_NODE = 1 
+  BNODE_LEAF = 2
+)
+
+func (node BNode) btype() uint16 {
+  return binary.LittleEndian.Uint16(node[0:2])
+}
+
+func (node BNode) nkeys() uint16 {
+  return binary.LittleEndian.Uint16(node[2:4])
+}
+
+func (node BNode) setHeader(btype uint16, nkeys uint16) {
+  binary.LittleEndian.PutUint16(node[0:2], btype)
+  binary.LittleEndian.PutUint16(node[2:4], nkeys)
+}
+
+func (node BNode) getPtr(idx uint16) uint64 {
+  assert(idx < node.nkeys())
+  pos := HEADER + 8*idx 
+  return binary.LittleEndian.Uint64(node[pos:])
+}
+func (node BNode) setPtr(idx uint64, val uint64)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 func SaveData(path string, data []byte) error {
   tmp := fmt.Sprintf("%s.tmp.%d", path, randomInt())
